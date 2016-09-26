96f36b54f285d8ea6ada02f6be4e6c17f105d663
rzese <riccardo.zese@unife.it>
2016-09-26 17:47:04 +0200

    pruned wrong explanations - TO TEST -

--- prolog/prova/metainterprete_new.pl
+++ prolog/prova/metainterprete_new.pl
@@ -12,11 +12,11 @@ lp_assertion_to_atom(lpPropertyAssertion(Role,Individual1, Individual2),Atom):-
 isSameIndividual(lpClassAssertion(_,Individual)):-
                 Individual=..[sameIndividual|_].
  
-solveNewGoals([],_,[]).
-solveNewGoals([H|T],G,E):-
+solveNewGoals([],_,GS,GS,[]).
+solveNewGoals([H|T],G,GAS,GS,E):-
                 H \= G,
-                solvei(H,EH),
-                solveNewGoals(T,G,ET),
+                solvei(H,GAS,GSH,EH),
+                solveNewGoals(T,G,GSH,GS,ET),
                 append(EH,ET,E).
 
 solve(G) :-
@@ -24,46 +24,59 @@ solve(G) :-
   retractall(ind(_)),
   build_and_expand(_),
   %assert(abox(A)),
-  solvei(G,E),
+  solvei(G,[],GS,E),
   list_to_set(E,Ex),
-  write(Ex).
+  write(Ex),
+  write(GS).
  
-solvei(true,[]):-!.
+solvei(true,GS,GS,[]):-!.
 
 % gestione clausole a(X):-b(X,Y),c(Y)
-solvei((A,B),E):-
+solvei((A,B),GAS,GS,E):-
 		A=..[R,I,Y],
 		B=..[C,Y],
 		instanceOf_meta(someValuesFrom(R,C),I,Explanation),
-		include(is_lp_assertion,Explanation,LPAssertions),
-                maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,(A,B),Ex),
+		   include(is_lp_assertion,Explanation,LPAssertions),
+                   maplist(lp_assertion_to_atom,LPAssertions,Atoms),
+                   solveNewGoals(Atoms,(A,B),GAS,GSNG,Ex),
+                   GS = [someValuesFrom(R,C)|GSNG],
                 append(Explanation,Ex,E).
 
-solvei((A,B),E):-!,
-                solvei(A,EA),
-                solvei(B,EB),
+solvei((A,B),GAS,[B|GS],E):-!,
+                solvei(A,GAS,GSA,EA),
+                solvei(B,[A|GSA],GS,EB),
                 append(EA,EB,E).
 
 
-solvei(nbf(Goal),['toImplement'|E]):-
+solvei(nbf(Goal),GAS,GS,['toImplement'|E]):-
                 !,
-                \+(solvei(Goal),E).
+                \+(solvei(Goal),GAS,GS,E).
                 
-solvei(Goal,[(Goal:-Body)|E]):-
-                clause(Goal,Body),
-                solvei(Body,E).
+solvei(Goal,GAS,GS,E):-
+                member(Goal,GAS) ->
+                  GS=GAS,E=[]
+                 ;
+                  (clause(Goal,Body),
+                   solvei(Body,[Goal|GAS],GS,Ex),
+                   E = [(Goal:-Body)|Ex]
+                  ).
 
-solvei(Goal,E):-
+solvei(Goal,GAS,GS,E):-
                 Goal=..[Class,Individual],
-                instanceOf_meta(Class,Individual,Explanation),
-                include(is_lp_assertion,Explanation,LPAssertions),
-                maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,Goal,Ex),
+                member(instanceOf(Class,Individual),GAS) ->
+                  GS = GAS, E = []
+                 ;
+                  (Goal=..[Class,Individual],
+                   instanceOf_meta(Class,Individual,Explanation),
+                   include(is_lp_assertion,Explanation,LPAssertions),
+                   maplist(lp_assertion_to_atom,LPAssertions,Atoms),
+                   solveNewGoals(Atoms,Goal,GAS,GSNG,Ex),
+                   GS=[instanceOf(Class,Individual)|GSNG]
+                ),
                 append(Explanation,Ex,E).
 
 
-solvei(Goal,[subClassOf(SubClass,Class)|E]):-
+solvei(Goal,GAS,GS,[subClassOf(SubClass,Class)|E]):-
 		Goal=..[Class,Individual],
 		owl2_model:subClassOf(SubClass,Class),
 		%( atom(SubClass) ->
@@ -73,10 +86,11 @@ solvei(Goal,[subClassOf(SubClass,Class)|E]):-
                 %  ;
                 %  (
                  \+ atom(SubClass),
-                   solveii(SubClass,Individual,E).
+                   solveii(SubClass,Individual,GAS,GSI,E),
+                   GS = [subClassOf(SubClass,Class)|GSI].
                 %  )
                 %).
-solvei(Goal,[equivalentClasses(L)|E]):-
+solvei(Goal,GAS,GS,[equivalentClasses(L)|E]):-
 		Goal=..[Class,Individual],
 		owl2_model:equivalentClasses(L),
 		member(Class,L),
@@ -89,7 +103,8 @@ solvei(Goal,[equivalentClasses(L)|E]):-
                 %  ;
                 %  (
                   \+ atom(SubClass),
-                   solveii(SubClass,Individual,E).
+                   solveii(SubClass,Individual,GAS,GSI,E),
+                   GS = [equivalentClasses(L)|GSI].
                 %  )
                 %). 
 
@@ -110,41 +125,44 @@ solvei(Goal):-
                 NewGoal=..[SubProp,I1,I2],
                 solvei(NewGoal). 
 */
-solvei(Goal,E):-
+solvei(Goal,GAS,GS,E):-
 		Goal=..[Role,Individual1,Indovidual2],
 		property_value_meta(Role,Individual1,Indovidual2,Explanation),
-                include(is_lp_assertion,Explanation,LPAssertions),
-                maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,Goal,Ex),
+                   include(is_lp_assertion,Explanation,LPAssertions),
+                   maplist(lp_assertion_to_atom,LPAssertions,Atoms),
+                   solveNewGoals(Atoms,Goal,GAS,GSNG,Ex),
+                   GS=[propertyAssertion(Role,Individual1,Indovidual2)|GSNG],
                 append(Explanation,Ex,E).
 
 % queste forse saranno da migliorare quando si calcolerà la probabilità
 % se infatti dobbiamo calcolare la probabilità di a(X,Y):-b(X,Y) ci sarà da gestire il caso di individui anonimi
 % creati dalla exists_rule
 %concept for concepts allValuesFrom
-solveii(allValuesFrom(R,C),I,E):-
+solveii(allValuesFrom(R,C),I,GAS,GS,E):-
   H=..[C,_],
   clause(H,B),
   B=..[D,_], %% d estendere a clausole con più congiunti nel corpo
   instanceOf_meta(allValuesFrom(R,D),I,Explanation),
                 include(is_lp_assertion,Explanation,LPAssertions),
                 maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,allValuesFrom(R,C),Ex),
-                append(Explanation,Ex,E).
+                solveNewGoals(Atoms,allValuesFrom(R,C),GAS,GSNG,Ex),
+                GS=[allValuesFrom(R,D)|GSNG],
+  append(Explanation,Ex,E).
 
 %role for concepts allValuesFrom
-solveii(allValuesFrom(R,C),I,E):-
+solveii(allValuesFrom(R,C),I,GAS,GS,E):-
   H=..[R,_,_],
   clause(H,B),
   B=..[S,_,_],
   instanceOf_meta(allValuesFrom(S,C),I,Explanation),
                 include(is_lp_assertion,Explanation,LPAssertions),
                 maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,allValuesFrom(R,C),Ex),
-                append(Explanation,Ex,E).
+                solveNewGoals(Atoms,allValuesFrom(R,C),GAS,GSNG,Ex),
+                GS=[allValuesFrom(S,C)|GSNG],
+  append(Explanation,Ex,E).
   
 %concept and role for concepts allValuesFrom
-solveii(allValuesFrom(R,C),I,E):-
+solveii(allValuesFrom(R,C),I,GAS,GS,E):-
   H=..[R,_,_],
   clause(H,B),
   B=..[S,_,_],
@@ -154,33 +172,36 @@ solveii(allValuesFrom(R,C),I,E):-
   instanceOf_meta(allValuesFrom(S,D),I,Explanation),
                 include(is_lp_assertion,Explanation,LPAssertions),
                 maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,allValuesFrom(R,C),Ex),
-                append(Explanation,Ex,E).
+                solveNewGoals(Atoms,allValuesFrom(R,C),GAS,GSNG,Ex),
+                GS=[allValuesFrom(S,D)|GSNG],
+  append(Explanation,Ex,E).
 
 %concept for concepts someValuesFrom
-solveii(someValuesFrom(R,C),I,E):-
+solveii(someValuesFrom(R,C),I,GAS,GS,E):-
   H=..[C,_],
   clause(H,B),
   B=..[D,_], %% d estendere a clausole con più congiunti nel corpo
   instanceOf_meta(someValuesFrom(R,D),I,Explanation),
                 include(is_lp_assertion,Explanation,LPAssertions),
                 maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,someValuesFrom(R,C),Ex),
-                append(Explanation,Ex,E).
+                solveNewGoals(Atoms,someValuesFrom(R,C),GAS,GSNG,Ex),
+                GS=[someValuesFrom(R,D)|GSNG],
+  append(Explanation,Ex,E).
 
 %role for concepts someValuesFrom
-solveii(someValuesFrom(R,C),I,E):-
+solveii(someValuesFrom(R,C),I,GAS,GS,E):-
   H=..[R,_,_],
   clause(H,B),
   B=..[S,_,_],
   instanceOf_meta(someValuesFrom(S,C),I,Explanation),
                 include(is_lp_assertion,Explanation,LPAssertions),
                 maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,someValuesFrom(R,C),Ex),
-                append(Explanation,Ex,E). 
+                solveNewGoals(Atoms,someValuesFrom(R,C),GAS,GSNG,Ex),
+                GS=[someValuesFrom(S,C)|GSNG],
+  append(Explanation,Ex,E). 
 
 %concept and role for concepts someValuesFrom
-solveii(someValuesFrom(R,C),I,E):-
+solveii(someValuesFrom(R,C),I,GAS,GS,E):-
   H=..[R,_,_],
   clause(H,B),
   B=..[S,_,_],
@@ -190,8 +211,9 @@ solveii(someValuesFrom(R,C),I,E):-
   instanceOf_meta(someValuesFrom(S,D),I,Explanation),
                 include(is_lp_assertion,Explanation,LPAssertions),
                 maplist(lp_assertion_to_atom,LPAssertions,Atoms),
-                solveNewGoals(Atoms,someValuesFrom(R,C),Ex),
-                append(Explanation,Ex,E).
+                solveNewGoals(Atoms,someValuesFrom(R,C),GAS,GSNG,Ex),
+                GS=[someValuesFrom(S,D)|GSNG],
+  append(Explanation,Ex,E).
                 
 /*****************************
   IDEA:
